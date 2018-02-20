fb950af2bc9560d819262151a9bace707d15a0d9
Margareta <margareta.vi@gmail.com>
2018-02-20 16:05:12 +0100

    nothing big

--- randomPick.py
+++ randomPick.py
@@ -3,33 +3,52 @@ from numpy import random
 
 def main(sys):
 	input_folder = os.path.abspath(sys.argv[1])
-	output_folder = os.path.abspath(sys.argv[2])
-	os.makedirs(output_folder, exist_ok=True)
-	how_many_percentage = float(sys.argv[3])
-
-	for file in os.listdir(input_folder):
-		if file == "groundTruth":
+	#output_folder = os.path.abspath(sys.argv[2])
+	#os.makedirs(output_folder, exist_ok=True)
+	#how_many_percentage = float(sys.argv[3])
+	how_many_percentage = 0.1
+	for subdir in os.listdir(input_folder):
+		if subdir != "randomHandel":
 			continue
-		directory = os.path.join(input_folder, file)
+		#if subdir == "groundTruth":
+		#	continue
+		directory = os.path.join(input_folder, subdir)
 		if os.path.isdir(directory):
 			directory = directory + '/'
-
 			filecount(os.path.abspath(directory))
 			directory_full_path = os.path.abspath(directory)			
 			number_of_files_to_move = math.ceil(filecount(directory_full_path)*how_many_percentage)
-			filenames = random.choice(os.listdir(directory_full_path), number_of_files_to_move)		
+			tmp_list = os.listdir(directory_full_path)
+			list_of_files = []
+			for file in tmp_list:
+				if file.endswith(".jpeg"):
+					list_of_files.append(file)
+			filenames = random.choice(list_of_files, number_of_files_to_move)		
+			lst = []
 			for name in filenames:
-				scrpath = os.path.join(directory_full_path,name)
-				dstpath = os.path.join(os.path.abspath(output_folder),name)
-				try:
-					shutil.move(scrpath, dstpath)
-				except:
-					continue	
+				if name.endswith(".jpeg"):
+					lst.append(name)
+			scrpath = os.path.join(directory_full_path,name)
+			dstpath = os.path.join(os.path.abspath(output_folder) + '/' ,name)
+			try:
+				shutil.copyfile(scrpath, dstpath)
+			except:
+				continue
+						
 
 # counts files in a folder
 def filecount(folder_path):
 	numfiles = sum(1 for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, f)))
 	return numfiles
+"""
+def move_copy_files(directory_full_path, name, output_folder):
+	scrpath = os.path.join(directory_full_path,name)
+	dstpath = os.path.join(os.path.abspath(output_folder) + '/' ,name)
+	try:
+		shutil.copyfile(scrpath, dstpath)
+	except:
+		continue
+"""
 
 if __name__ == "__main__":
     main(sys)
